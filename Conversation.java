6fb689d6a31409162afef085eddb4a7a08cb679e
Tseegi_N <tnyamdorj@smith.edu>
2022-09-22 00:33:55 -0400

    Introduced data, started on Conversation

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 class Conversation {
 
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    Scanner in;
+    Integer round;
+    String first_reply;
+    
+    in = new Scanner(System.in);
+    round = in.nextInt();
+
+    System.out.println("Hi there!  What's on your mind?");
+    in = new Scanner(System.in);
+    first_reply = in.nextLine();
+    //Get substrings and check if any of the keywords exist
+    
+
   }
 }
